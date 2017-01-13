8ea5c76a1d646f2ceb7510256c1a97f54a07fca3
Theodoros Antoniou <theodoros.antoniou@sgx.com>
2017-01-13 15:13:42 +0800

    enable cors

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var app = express()
+var cors = require('cors')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello kokos!')
+})
+
+app.options('/preflight', cors({
+  origin: true,
+  credentials: true
+}));
+
+app.get('/preflight', function(req, res) {
+  res.json({
+    text: 'Complex CORS requests are working.'
+  });
 })
 
 app.listen(app.get('port'), function() {
